cbc9b99e61c81c5d8e07f2b318ce7e8b3eab6bf2
ttahm3d <ttahm3d@gmail.com>
2021-12-31 19:08:54 +0530

    feat: created about page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Container } from "../src/utils/styles/globals";
+
+const About: NextPage = () => {
+	return (
+		<Container>
+			<h3>About page</h3>
+		</Container>
+	);
+};
+
+export default About;
