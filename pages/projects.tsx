46f847f752e4badad17f8c9c8a5e67e82ad4f236
ttahm3d <ttahm3d@gmail.com>
2022-01-06 17:06:10 +0530

    feat: added projects page content

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,29 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Container } from "../src/utils/styles/globals";
+import {
+	Container,
+	HeroSection,
+	Paragraph,
+	CenterContent,
+	Title,
+} from "../src/utils/styles/globals";
 
 const Projects: NextPage = () => {
-	return <Container>projects page</Container>;
+	return (
+		<Container>
+			<Head>
+				<title>Projects | Tahir Ahmed</title>
+			</Head>
+			<HeroSection>
+				<CenterContent>
+					<Title>
+						<span>projects</span>
+					</Title>
+				</CenterContent>
+			</HeroSection>
+			<Paragraph>Coming soon</Paragraph>
+		</Container>
+	);
 };
 
 export default Projects;
