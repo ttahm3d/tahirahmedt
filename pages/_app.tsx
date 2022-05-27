d95400360f884c953679b1305e50ccb91e8c5d6c
ttahm3d <ttahm3d@gmail.com>
2022-05-27 12:25:48 +0530

    feat: added new fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Page } from "../styles/globals";
 import { useState } from "react";
 import { ThemeProvider, DefaultTheme } from "styled-components";
-import { GlobalStyles, LightTheme, DarkTheme } from "../styles/theme";
+import { GlobalStyles } from "../styles";
+import { LightTheme, DarkTheme } from "../styles/theme";
 import Footer from "../src/components/Footer/Footer";
+import Head from "next/head";
 import Header from "../src/components/Header/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -16,6 +17,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <GlobalStyles />
       <Page>
         <Header theme={theme.title} toggleTheme={toggleTheme} />
