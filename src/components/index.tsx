83571779e4da09e283c8d38f518750c096a43ca6
ttahm3d <ttahm3d@gmail.com>
2025-10-17 21:12:14 +0530

    feat - deleted old files

--- src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Footer from './common/Footer/Footer'
-import HashnodeBlogs from './common/HashnodeBlogs/HashnodeBlogs'
-import Header from './common/Header/Header'
-import Hero from './common/Hero/Hero'
-import Paragraph from './UI/Paragraph/Paragraph'
-import Skills from './common/Skills/Skills'
-import TimelineCard from './UI/TimelineCard/TimelineCard'
-
-export { Footer, HashnodeBlogs, Header, Hero, Paragraph, Skills, TimelineCard }
